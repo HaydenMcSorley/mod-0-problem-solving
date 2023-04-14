270dc39d3586be34c50376fd942eb5435d5fde66
Hayden McSorley <haymcsorley@gmail.com>
2023-04-14 15:38:37 -0500

    add another way to solve third question

--- problem_solving.rb
+++ problem_solving.rb
@@ -20,9 +20,21 @@
 
 #Given a sentence with only lowercase letters, print the same sentence with the first letter of every word capitalized. For example, if you were given "Turing is the best", return "Turing Is The Best" instead!
 
+# def capitalized_words(sentence)
+#     words = sentence.split(" ")
+#     capitalized_words = words.map { |word| word.capitalize}
+#     capitalized_words.join(" ")
+# end
+# p capitalized_words("This is a sentence.")
+
 def capitalized_words(sentence)
     words = sentence.split(" ")
-    capitalized_words = words.map { |word| word.capitalize}
-    capitalized_words.join(" ")
-end
+    capitalized_words = []
+    words.each do |word|
+       capitalized_words.push(word.capitalize)
+    end   
+    final_sentence = capitalized_words.join(" ")
+    return final_sentence
+end    
+
 p capitalized_words("This is a sentence.")
